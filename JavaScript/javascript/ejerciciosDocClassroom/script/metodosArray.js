69ef292c75d735fcbe230fb0bdaed8af9d43785c
Ivan <ivantellez04@iesamachado.org>
2024-01-21 19:08:57 +0100

    intentos de metodos array

---
+++ JavaScript/javascript/ejerciciosDocClassroom/script/metodosArray.js
@@ -0,0 +1,7 @@
+function add(){
+     var Array = [];
+     var elemento = document.getElementById(nuevoElemento).value;
+    Array.push(elemento);
+    document.write('Se ha añadido correctamente la palabra '+ Array[elemento]);
+}
+
