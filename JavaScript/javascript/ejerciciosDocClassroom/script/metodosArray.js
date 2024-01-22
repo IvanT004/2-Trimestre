61e36399cbe4aec0e7150f2f0166efdd46517ea5
Ivan <ivantellez04@iesamachado.org>
2024-01-22 20:15:18 +0100

    lo siento joseluis mi cabeza está 💀🤯

--- JavaScript/javascript/ejerciciosDocClassroom/script/metodosArray.js
+++ JavaScript/javascript/ejerciciosDocClassroom/script/metodosArray.js
@@ -1,7 +1,29 @@
-function add(){
-     var Array = [];
-     var elemento = document.getElementById(nuevoElemento).value;
-    Array.push(elemento);
-    document.write('Se ha añadido correctamente la palabra '+ Array[elemento]);
+
+var ElementList = [""]
+
+function add() {
+
+    var elemento = document.getElementById("introducido").value;
+    ElementList.push(elemento);
+    document.getElementById("escribir").innerHTML = ElementList;
+}
+
+function remove(){
+
+    var elemento = document.getElementById("introducido").value;
+    ElementList.shift(elemento);
+    document.getElementById("escribir").innerHTML = ElementList;
+}
+
+function dec(){
+
+        ElementList.sort(function(a, b){return b - a});
+    
+}
+
+function crec(){
+
+    document.write(ElementList.sort(function(a, b){return a - b}))
+
 }
 
