f5c2f015f9c75bb2a85c3d4d7be38878c860e9ab
Ivan Tellez Cugat <ivantellez04@iesamachado.org>
2024-01-25 13:29:02 +0100

    arrays reales

---
+++ JavaScript/javascript/ejerciciosClase/scripts/ArrayReales.js
@@ -0,0 +1,19 @@
+var Numeros =[];
+
+function anadir(){
+
+    var NewNumb = document.getElementById("new").value;
+    Numeros.push(NewNumb);
+    
+}
+
+function operar(){
+
+    for(i=0; i<=Numeros.length; i++){
+        
+        sumador=Numeros[i];
+        
+        }
+
+        acumulador = sumador + 
+}
