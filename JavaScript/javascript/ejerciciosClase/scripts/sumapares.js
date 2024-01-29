68c9e86d2385a24ead4927e88a2c2bd04db4087f
Ivan <ivantellez04@iesamachado.org>
2024-01-29 17:52:16 +0100

    sumaPares(hecho)

--- JavaScript/javascript/ejerciciosClase/scripts/sumapares.js
+++ JavaScript/javascript/ejerciciosClase/scripts/sumapares.js
@@ -1,33 +1,28 @@
-var numeros = ["1","8","16","5"]
-contador = 0;
+var numeros = [1,8,16,5]
+
 
 function guardar(){
-
-    newElem = document.getElementById("introducir").value ;
-    parseFloat(numeros.push(newElem));
+    
+    
+    newElem = parseInt(document.getElementById("introducir").value) ;
+    numeros.push(newElem);
     document.getElementById("comunicar").innerHTML= "Se ha introducido correctamente el número "+ newElem + " en el Array";
+   
+
 }
 
 
 function sumar(){
-
+    contador = 0;
     for(i=0; i < numeros.length; i++){
 
-        while(numeros[i] % 2 == 0){
-
-            var Pares =[];
-            Pares.push(numeros [i]);
+        if (numeros[i] % 2 == 0) {
+            
+            suma = contador + numeros[i];
+            contador = suma;
         }
         
     } 
-
-    while(i < Pares.length){
-
-            acumulador = Pares[j];
-            
-            
-        }
-         
-
+        document.getElementById("comunicar").innerHTML = "La suma de los numeros pares del array da como resultado: "+ suma;
 }
 
