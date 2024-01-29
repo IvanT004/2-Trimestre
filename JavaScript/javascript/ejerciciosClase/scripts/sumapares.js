acae9ac66ece7cbd0d27e19d7e1c138640423a87
Ivan Tellez Cugat <ivantellez04@iesamachado.org>
2024-01-29 09:00:48 +0100

    sumapares

--- JavaScript/javascript/ejerciciosClase/scripts/sumapares.js
+++ JavaScript/javascript/ejerciciosClase/scripts/sumapares.js
@@ -1,7 +1,33 @@
-var numeros = []
+var numeros = ["1","8","16","5"]
+contador = 0;
 
 function guardar(){
 
-    nuevo = numeros.push();
-    document.getElementById("introducir").value.inne
+    newElem = document.getElementById("introducir").value ;
+    parseFloat(numeros.push(newElem));
+    document.getElementById("comunicar").innerHTML= "Se ha introducido correctamente el número "+ newElem + " en el Array";
 }
+
+
+function sumar(){
+
+    for(i=0; i < numeros.length; i++){
+
+        while(numeros[i] % 2 == 0){
+
+            var Pares =[];
+            Pares.push(numeros [i]);
+        }
+        
+    } 
+
+    while(i < Pares.length){
+
+            acumulador = Pares[j];
+            
+            
+        }
+         
+
+}
+
