5e1a1aba6cdaeddf42d399f662bf19b8dfb67c48
Ivan Tellez Cugat <ivantellez04@iesamachado.org>
2024-02-01 13:24:57 +0100

    s

--- JavaScript/javascript/ejerciciosDocClassroom/script/validar.js
+++ JavaScript/javascript/ejerciciosDocClassroom/script/validar.js
@@ -3,7 +3,7 @@ function validar(elementos){
     let estanCorrectos = true;
 
     for(var i=0; i<elementos.length;i++){
-        if(elementos[i].value == "" || elementos[i]== "off"){
+        if(elementos[i].value == "" || !elementos[i].checked){
         document.getElementById("campo"+(i+1).toString()).innerHTML = "El campo " + elementos[i].id + " está vacio"
         estanCorrectos = false;
     }
